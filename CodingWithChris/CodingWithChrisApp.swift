9468142c475afb0043e56abb5ed4a9093b158023
bmoxey <bmoxey@gmail.com>
2023-08-04 08:26:47 +1000

    Initial Commit

---
+++ CodingWithChris/CodingWithChrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingWithChrisApp.swift
+//  CodingWithChris
+//
+//  Created by Brett Moxey on 4/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodingWithChrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
