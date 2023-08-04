11a6ff42087e90b524c087de7921f9f3ee1fa19b
bmoxey <bmoxey@gmail.com>
2023-08-04 11:24:26 +1000

    Completed UI

--- CodingWithChris/ContentView.swift
+++ CodingWithChris/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            ContainerRelativeShape()
+                .fill(Color.green.gradient)
+                .ignoresSafeArea()
+            VStack {
+                Image("war")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top, 50.0)
+                    .frame(width: 150)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120)
+                    Spacer()
+                    Image("card4")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120)
+                    Spacer()
+                }
+                Spacer()
+                Image("deal")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150)
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.bottom, 10.0/*@END_MENU_TOKEN@*/)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(/*@START_MENU_TOKEN@*/.bottom, 10.0/*@END_MENU_TOKEN@*/)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
+
+        
     }
 }
 
